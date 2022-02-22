c8b5897d1481bfcbb078734e94e25decbd51fa78
20FTT1464 <20ftt1464@student.pb.edu.bn>
2022-02-22 13:16:27 +0800

    Layouting
    no function yet

--- GP7AWEP/index.php
+++ GP7AWEP/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <style>
-            
-        </style>
+        <link rel="stylesheet" href="assets/css/base.css"></link>
     </head>
+
     <body>
-        
+        <div class="container">
+            <img src="" alt="">
+            <button href="/">Choose Room</button>
+            <br>
+            <button href="/">Check In</button>
+        </div>
     </body>
 </html>
