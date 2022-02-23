e2098fa9acc4ed7a0bfeac4bbcde8bf3ec850775
20FTT1464 <20ftt1464@student.pb.edu.bn>
2022-02-23 13:51:03 +0800

    Update choose-room.php

--- GP7AWEP/choose-room.php
+++ GP7AWEP/choose-room.php
@@ -39,6 +39,7 @@
     <body>
         <div class="top-container"><b>Choose Room</b> > Book Form > Finished </div>
         <div class="container">
+<<<<<<< HEAD
             <div id="roomone" class="sub-con">
                 <label>Room 1</label>
                 <input type="radio" id value="8am-9am">8am-9am 
@@ -64,6 +65,14 @@
                 <label>Room 6</label>
             
             </div>
+=======
+            <div id="roomone" class="sub-con">Room 1</div>
+            <div id="roomtwo" class="sub-con">Room 2</div>
+            <div id="roomthree" class="sub-con">Room 3</div>
+            <div id="roomfour" class="sub-con">Room 4</div>
+            <div id="roomfive" class="sub-con">Room 5</div>
+            <div id="roomsix" class="sub-con">Room 6</div>
+>>>>>>> parent of e7460b4 (Update choose-room.php)
         </div>
     </body>
 </html>
