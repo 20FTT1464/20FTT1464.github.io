f062911ed37eea6a1085ff38fba7afa838b93de3
20FTT1464 <20ftt1464@student.pb.edu.bn>
2022-02-23 13:51:25 +0800

    Update choose-room.php

--- GP7AWEP/choose-room.php
+++ GP7AWEP/choose-room.php
@@ -40,6 +40,7 @@
         <div class="top-container"><b>Choose Room</b> > Book Form > Finished </div>
         <div class="container">
 <<<<<<< HEAD
+<<<<<<< HEAD
             <div id="roomone" class="sub-con">
                 <label>Room 1</label>
                 <input type="radio" id value="8am-9am">8am-9am 
@@ -66,13 +67,18 @@
             
             </div>
 =======
+=======
+>>>>>>> parent of 9a4362c (choose-room update)
             <div id="roomone" class="sub-con">Room 1</div>
             <div id="roomtwo" class="sub-con">Room 2</div>
             <div id="roomthree" class="sub-con">Room 3</div>
             <div id="roomfour" class="sub-con">Room 4</div>
             <div id="roomfive" class="sub-con">Room 5</div>
             <div id="roomsix" class="sub-con">Room 6</div>
+<<<<<<< HEAD
 >>>>>>> parent of e7460b4 (Update choose-room.php)
+=======
+>>>>>>> parent of 9a4362c (choose-room update)
         </div>
     </body>
 </html>
