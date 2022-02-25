19502f0844e1dc423b5e484a029dd00a5c01a60d
HisyamRosli <82690198+HisyamRosli@users.noreply.github.com>
2022-02-25 20:32:26 +0800

    array change

--- GP7AWEP/checkin.php
+++ GP7AWEP/checkin.php
@@ -22,67 +22,112 @@
         array(
             'room'=>'2',
             'time'=>'1.30pm-2.30pm',
-            'id'=>'19FTT2932',
+            'no_people'=>'4',
+            'id'=>'19FTT4321',
+            'id'=>'19FTT4329',
+            'id'=>'19FTT9435',
+            'id'=>'19FTT9432',
 
         ),
         array(
             'room'=>'3',
             'time'=>'1.30pm-2.30pm',
-            'id'=>'19FTT3939',
+            'no_people'=>'5',
+            'id'=>'19FTT2124',
+            'id'=>'19FTT2366',
+            'id'=>'19FTT9034',
+            'id'=>'19FTT7456',
+            'id'=>'19FTT1234',
+
 
         ),
         array(
             'room'=>'4',
             'time'=>'1.30pm-2.30pm',
-            'id'=>'19FTT4034',
+            'no_people'=>'3',
+            'id'=>'19FTT4589',
+            'id'=>'19FTT9843',
+            'id'=>'19FTT8712',
+
 
         ),
         array(
             'room'=>'5',
             'time'=>'1.30pm-2.30pm',
-            'id'=>'19FTT6903',
+            'no_people'=>'4',
+            'id'=>'19FTT9328',
+            'id'=>'19FTT2374',
+            'id'=>'19FTT0921',
+            'id'=>'19FTT9213',
 
         ),
         array(
             'room'=>'6',
             'time'=>'1.30pm-2.30pm',
-            'id'=>'19FTT3023',
+            'no_people'=>'5',
+            'id'=>'19FTT6435',
+            'id'=>'19FTT6546',
+            'id'=>'19FTT7564',
+            'id'=>'19FTT2134',
+            'id'=>'19FTT2133',
 
         ),
         array(
             'room'=>'1',
             'time'=>'2.30pm-3.30pm',
-            'id'=>'20FTT4409',
+            'no_people'=>'3',
+            'id'=>'20FTT3452',
+            'id'=>'20FTT8434',
+            'id'=>'20FTT5456',
 
         ),
         array(
             'room'=>'2',
             'time'=>'2.30pm-3.30pm',
-            'id'=>'20FTT3922',
+            'no_people'=>'4',
+            'id'=>'20FTT2732',
+            'id'=>'20FTT7234',
+            'id'=>'20FTT3472',
+            'id'=>'20FTT5212',
+
 
         ),
         array(
             'room'=>'3',
             'time'=>'2.30pm-3.30pm',
-            'id'=>'20FTT9323',
-
+            'no_people'=>'5',
+            'id'=>'20FTT3923',
+            'id'=>'20FTT7235',
+            'id'=>'20FTT8244',
+            'id'=>'20FTT0421',
+            'id'=>'20FTT2193',
         ),
         array(
             'room'=>'4',
             'time'=>'2.30pm-3.30pm',
-            'id'=>'20FTT0232',
-
+            'no_people'=>'3',
+            'id'=>'20FTT3249',
+            'id'=>'20FTT4024',
+            'id'=>'20FTT0923',
         ),
         array(
             'room'=>'5',
             'time'=>"2.30pm-3.30pm",
-            'id'=>'20FTT8341',
-
+            'no_people'=>'4',
+            'id'=>'20FTT5702',
+            'id'=>'20FTT0235',
+            'id'=>'20FTT3214',
+            'id'=>'20FTT5613',
         ),
         array(
             'room'=>'6',
             'time'=>"2.30pm-3.30pm",
-            'id'=>'20FTT6535',
+            'no_people'=>'5',
+            'id'=>'20FTT4983',
+            'id'=>'20FTT2346',
+            'id'=>'20FTT4589',
+            'id'=>'20FTT7835',
+            'id'=>'20FTT2398',
 
         ),
         );
